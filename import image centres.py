2c9def63c43010f75bcf9f63556386da1f3fa607
gangotricertification <46564926+gangotricertification@users.noreply.github.com>
2019-01-10 19:27:28 +0530

    Add files via upload

---
+++ import image centres.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from reference_OMR import reference_image
+from student_OMR import student_image
+
+image1=cv2.imread(r"D:\project\OMR\aligned.jpg")
+image2=cv2.imread(r"D:\project\OMR\images\new omr\new2.png")
+
+r_data=reference_image(image1)
+s_data=student_image(image2)
+
+#print("reference OMR centres : \n",r_data)
+#print("student OMR centres : \n",s_data)
+
+count=0
+for i in range(1,21,1):
+    a=np.sum(np.square((r_data[i]-s_data[i])),axis=0)
+    #print(a)
+    if a <= 49:
+        count=count+1
+
+print("total marks is : ",count)
+print(" GANGOTRI MISHRA OFFICIAL (gangotricertification)")
